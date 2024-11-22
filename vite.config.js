caeb92d0cb25f32ff5acc154794420bbf824a94d
Gilberto Sá <gibasa@gmail.com>
2024-11-22 07:42:18 -0300

    resolvendo problema img 5

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    assetsDir: 'assets', // Especifica onde os arquivos estáticos serão colocados no build
+    assetsInclude: ["**/*.jpg", "**/*.png"], // Especifica onde os arquivos estáticos serão colocados no build
   },
 })

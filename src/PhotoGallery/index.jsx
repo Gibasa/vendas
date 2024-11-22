54e6ebd0e79f4b98517b8efc66d82ddd6e3ffc96
Gilberto Sá <gibasa@gmail.com>
2024-11-22 07:38:54 -0300

    resolvendo problema img 5

--- src/PhotoGallery/index.jsx
+++ src/PhotoGallery/index.jsx
@@ -52,7 +52,7 @@ const PhotoGallery = () => {
 
   useEffect(() => {
     // Glob para carregar todas as imagens da pasta 'public/photos'
-    const images = import.meta.glob("/src/assets/images/*.jpg", { as: "url" });
+    const images = import.meta.glob("./assets/images/*.jpg", { as: "url" });
 
     const groups = {};
 
